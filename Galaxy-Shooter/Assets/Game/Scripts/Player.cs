c7a49950b4250146924a7432ed5ca8b7b7b9f484
sounritesh <sounritesh@gmail.com>
2020-06-08 16:11:42 +0530

    initial commit

---
+++ Galaxy-Shooter/Assets/Game/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start: called");
+        transform.position = new Vector3(0,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update: called");
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+    }
+}
